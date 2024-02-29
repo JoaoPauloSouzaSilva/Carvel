52ad59c896bb87d8a5b6bcc8069c2b762af4cf54
JoaoPauloSouzaSilva <joaopaulloss477@gmail.com>
2024-02-29 15:52:02 -0300

    Navigation Bar

---
+++ carvel/src/components/navbar/navbar.js
@@ -0,0 +1,33 @@
+import './navbar.css';
+
+function NavBar() {
+  return (
+    <div className="container">
+
+      <img id="logo" src="/logo.png" alt="Logo Carvel" />
+
+
+      <div className="container-options-navigation">
+        <div className="options-navigation">
+          <ul>
+            <li><button>Comprar</button></li>
+            <li><button>Vender</button></li>
+            <li><button>Serviços</button></li>
+          </ul>
+        </div>
+      </div>
+
+     <div className="option-login">
+        <button id="button-login">
+          <img id="icon-user" src="/user.png" alt="Login" />
+          <p>Entrar</p>
+        </button>
+        {/* https://www.flaticon.com/br/icone-gratis/do-utilizador_8238768?term=usuario&page=1&position=11&origin=search&related_id=8238768 */}
+     </div>
+
+
+    </div>
+  );
+}
+
+export default NavBar;
