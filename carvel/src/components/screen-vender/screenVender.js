c4221557b982c16865d082b44bf690ad99187331
JoaoPauloSouzaSilva <joaopaulloss477@gmail.com>
2024-03-01 14:25:29 -0300

    Screen Vender

--- carvel/src/components/screen-vender/screenVender.js
+++ carvel/src/components/screen-vender/screenVender.js
@@ -3,25 +3,26 @@ import './screenVender.css';
 function ScreenVender() {
   return (
     <div className="containerScreenVender">
-      <div className="mainScreenVender">
+      <div className="mainScreenVender01">
         <div className="left">
-
           <div className="containerTextScreenVender">
           <p id="textScreenVender">
             Somos o melhor lugar para você <mark> vender seu veiculo rápido</mark>.
           </p>
           </div>
-
           <button id="actionScreenVender">
             Vender Veiculo
-          </button>
-          
+          </button>  
         </div>
-
         <div className="right">
-          ola
+          <img id="carrosScreenVender" src="/carros.png" alt="." />
         </div>
       </div>
+
+      <div className="mainScreenVender02">
+
+      </div>
+
     </div>
   );
 }
